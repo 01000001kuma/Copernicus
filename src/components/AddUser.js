2ec237edf2277d591986fde3ce38edfc9a52e136
01000001kuma <a_illera@yahoo.com>
2023-04-29 14:51:12 +0200

    creating schema for user and api call

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const AddUser = () => {
+  
+  const addUser = () => {
+
+  }
+
+  
   return (
     <div className='container'>
         
@@ -10,7 +16,27 @@ const AddUser = () => {
 
         <div className='row'>
           <div className='col-sm-6 offset-3'>
-            <h1>Add User</h1>
+            <div className='mb-3'>
+              <label htmlFor='name' className='form-label'>Name</label>
+              <input type='text' className='form-control'></input>
+            </div> 
+            <div className='mb-3'>
+              <label htmlFor='last' className='form-label'>Last name</label>
+              <input type='text' className='form-control'></input>
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='email' className='form-label'>Email</label>
+              <input type='text' className='form-control'></input>
+            </div> 
+            <div className='mb-3'>
+              <label htmlFor='company' className='form-label'>Company</label>
+              <input type='text' className='form-control'></input>
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='country' className='form-label'>Country</label>
+              <input type='text' className='form-control'></input>
+            </div> 
+            <button onClick={addUser} className='btn btn-success'>Add</button>
           </div>
         </div>
 
