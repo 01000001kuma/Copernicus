33dcf3916ec45f65000609ebae2e0b3d46bde7a3
01000001kuma <a_illera@yahoo.com>
2023-04-29 14:27:29 +0200

    wip adding user table

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const AddUser = () => {
   return (
-    <div>
-        <h1>Add User</h1>
+    <div className='container'>
+        
+        <div className='row'>
+            <h2 className='mt-4'>Add User</h2>
+        </div>
+
+        <div className='row'>
+          <div className='col-sm-6 offset-3'>
+            <h1>Add User</h1>
+          </div>
+        </div>
+
     </div>
   )
 }
