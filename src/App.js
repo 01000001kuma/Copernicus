37728423115a45e584fc388032bda917a89ea9b3
01000001kuma <a_illera@yahoo.com>
2023-04-29 11:37:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import UserList from './components/UserList';
+import AddUser from './components/AddUser';
+import EditUser from './components/EditUser';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React App</h1>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<UserList/>} exact></Route>
+          <Route path='/adduser' element={<AddUser/>}exact></Route>
+          <Route path='/edituser' element={<EditUser/>}exact></Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/UserList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserDetail from './UserDetail'
+
+const UserList = () => {
+  return (
+    <>
+    <div>userList</div>
+    <UserDetail />    
+    </>
+  )
+}
+
+export default UserList
