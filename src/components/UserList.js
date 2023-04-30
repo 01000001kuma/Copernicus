140af36dd00077db62559125b93bff966e96025a
01000001kuma <a_illera@yahoo.com>
2023-04-30 11:56:19 +0200

    setting pagination to deal with loading speed

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import UserDetail from './UserDetail';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import UserDetail from './UserDetail';
 
 const UserList = () => {
   const [dataUsers, setDataUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
 
   useEffect(() => {
     axios
-      .get('/api/user/list')
+      .get(`/api/user/list?page=${currentPage}`)
       .then((res) => {
         console.log(res.data);
-        setDataUsers(res.data);
+        setDataUsers(res.data.users);
+        setTotalPages(res.data.totalPages);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [currentPage]);
+
+  const handlePrevPage = () => {
+    setCurrentPage(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   const userList = dataUsers.map((user) => {
     return (
@@ -30,6 +40,12 @@ const UserList = () => {
     <>
       <h2>UserList</h2>
       {userList}
+      {currentPage > 1 && (
+        <button onClick={handlePrevPage}>Previous</button>
+      )}
+      {totalPages && currentPage < totalPages && (
+        <button onClick={handleNextPage}>Next</button>
+      )}
     </>
   );
 };
